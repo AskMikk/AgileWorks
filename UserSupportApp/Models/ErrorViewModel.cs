243e72ec64d04ff9f901bf275982728124e1e96a
Mükail Askerov <90192553+AskMikk@users.noreply.github.com>
2024-04-06 00:22:39 +0300

    .NET init

---
+++ UserSupportApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UserSupportApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ UserSupportApp/Program.cs
@@ -0,0 +1,28 @@
+using UserSupportApp.Interfaces;
+using UserSupportApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Inquiries}/{action=Index}/{id?}");
+
+app.Run();
